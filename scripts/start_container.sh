6b9be2c78b96934e668fc8e3c9ab7c10d93e1482
ishika-iisc <ishika.innovative@gmail.com>
2026-02-15 12:44:48 +0530

    update scripts

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,8 +1,31 @@
 #!/bin/bash
-set -e
 echo "Hi"
-# Pull the Docker image from Docker Hub
-docker pull $DOCKER_REGISTRY_USERNAME/python-flask-aws:latest
 
-# Run the Docker image as a container
-docker run -d -p 5000:5000 $DOCKER_REGISTRY_USERNAME/python-flask-aws:latest
+REGION="ap-south-1"
+
+# Get credentials from SSM
+DOCKER_USERNAME=$(aws ssm get-parameter \
+  --name "/learnbayapp/docker/username" \
+  --region $REGION \
+  --query "Parameter.Value" \
+  --output text)
+
+DOCKER_PASSWORD=$(aws ssm get-parameter \
+  --name "/learnbayapp/docker/password" \
+  --with-decryption \
+  --region $REGION \
+  --query "Parameter.Value" \
+  --output text)
+
+# Login to Docker Hub
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
+# Pull latest image
+docker pull motieno205/python-flask-app:latest
+
+# Stop old container (if exists)
+docker stop flask-app || true
+docker rm flask-app || true
+
+# Run container
+docker run -d -p 5000:5000 --name flask-app motieno205/python-flask-app:latest
